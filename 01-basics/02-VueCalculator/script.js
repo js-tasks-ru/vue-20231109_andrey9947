72940084427a88b2112b59fffde1bd9a599f3d46
ArhStrAngeR <arhstranger@gmail.com>
2024-01-22 03:20:14 +0300

    solve 02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      num1: null,
+      num2: null,
+      typeAction: null,
+    };
+  },
+  computed: {
+    calculation() {
+      switch (this.typeAction) {
+        case 'sum':
+          return this.num1 + this.num2;
+        case 'subtract':
+          return this.num1 - this.num2;
+        case 'multiply':
+          return this.num1 * this.num2;
+        case 'divide':
+          return this.num1 / this.num2;
+        default:
+          return null;
+        }
+    },
+  },
+});
+app.mount('#app');
